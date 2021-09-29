956c6ff3d4a7d294e9b098cf0d768ff842ffc7fb
Mingw0717-Dell <xmw0517@gmail.com>
2021-09-29 11:06:12 +0800

    practice-01

--- test/src/com/atguigu/practice/practice-01.sql
+++ test/src/com/atguigu/practice/practice-01.sql
@@ -21,7 +21,7 @@ WHERE salary > 12000;
 
 
 # 4.查询员工号为176的员工的姓名和部门号
-SELECT last_name,job_id
+SELECT last_name,department_id
 FROM employees
 WHERE employee_id = 176;
 
@@ -53,7 +53,7 @@ WHERE department_id IN(20,50);
 
 
 
-# 7.选择公司中没有管理者的员工姓名及job_id		IS NULL 用来判断是为空
+# 7.选择公司中没有管理者的员工姓名及job_id		IS NULL 用来判断where后面的字段是否为空
 SELECT last_name,job_id
 FROM employees
 WHERE manager_id IS NULL;
@@ -62,11 +62,42 @@ WHERE manager_id IS NULL;
 
 
 # 8.选择公司中有奖金的员工姓名，工资和奖金级别
-
+SELECT employee_id,last_name,salary,commission_pct
+FROM employees
+WHERE commission_pct IS NOT NULL;
 
 
 
 
 # 9.选择员工姓名的第三个字母是a的员工姓名
+SELECT employee_id,last_name
+FROM employees
+WHERE last_name LIKE '__a%';
+
+
+
 
 # 10.选择姓名中有字母a和e的员工姓名
+SELECT employee_id,last_name,salary
+FROM employees
+WHERE last_name LIKE '%a%e%' OR last_name LIKE '%e%a%';
+# WHERE last_name LIKE '%a%e%' OR '%e%a%';		# 写成这样是不正确的, 会少数据. 原因待查
+
+/*
+	TODO:
+	以后再写这种查询, 不要把要用到的限制字段丢掉! 切记, 这可能会产生数据丢失. 但是又不报错, 莫名其妙的错误最难改!
+*/
+
+
+
+SELECT employee_id,last_name
+FROM employees
+WHERE last_name LIKE '%a%' AND last_name LIKE '%e%';
+
+
+
+
+
+
+
+
