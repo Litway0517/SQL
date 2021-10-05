29ff2e3ed372138b45bb18afa8091a60ef2767cf
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-05 16:51:00 +0800

    11-创建和管理表

--- test/src/com/atguigu/test/11-创建和管理表.sql
+++ test/src/com/atguigu/test/11-创建和管理表.sql
@@ -142,22 +142,84 @@ DROP TABLE employees_copy;
 
 
 /*
-    8. 清空表
+    8. 清空表 -> 清空表中的数据, 但是表的结构会保留
+*/
+SELECT *
+FROM myemp3;
+
+
+TRUNCATE TABLE myemp3;
+
+
+DESC myemp3;
+
+
+/*
+    对比 TRUNCATE 和 DELETE FROM...
+    结论 -> 
+	TRUNCATE TABLE 一旦操作, 就不可以回滚数据. 考虑清楚, 只有一次机会. 
+		TRUNCATE操作, 做完了之后就会COMMIT, 不会受到SET autocommit = FALSE;的影响. 
+	DELETE FROM 支持表中数据的全部删除, 可以回滚数据. 
+	
+    测试 -> COMMIT 与 ROLLBACK的使用
+	1. COMMIT表示提交数据. 一旦提交数据, 就不能再回滚. 
+		有点和Git commit机制差不多. 数据添加到本地库之后就直接生成hash标志码了, 不能在进行更改. 
+		
+		回滚操作只能回滚到最近的一次commit操作之后. 
+	2. ROLLBACK表示回滚数据. 
+	
+	但是, 默认情况下, DBMS会把 "默认提交" 这个开关打开. 
+	默认情况下, 对数据表的操作(DDL, DML), 都是在执行之后, 默认提交数据的. 
+	因此, 如果想测试TRUNCATE 与 DELETE FROM 的区别, 需要把这个开关关上.
+	set autocommit = false;
 */
 
+CREATE TABLE myemp
+AS
+SELECT *
+FROM employees;
 
 
 
+SELECT *
+FROM myemp;
 
 
+COMMIT;
 
 
+# 首先测试DELETE FROM
+SET autocommit = FALSE;
+
+DELETE FROM myemp;	# 删除数据
+
+SELECT *		# 查询数据
+FROM myemp;
+
+ROLLBACK;		# 回滚数据, 让刚才删除的数据再退回
 
 
+# 接着测试TRUNCATE TABLE语句
+COMMIT;
+
+SET autocommit = FALSE;
+
+TRUNCATE TABLE myemp;	# 清空表
+
+ROLLBACK;		# 回滚操作
+
+SELECT *
+FROM myemp;
 
 
-
-
+/*
+    结论 ->
+	一TRUNCATE TABLE为代表的DDL操作, 都会在执行了语句之后, 自动执行COMMIT提交数据. 
+	而且此行为提交不受SET autocommit = FALSE;语句限制. 所以, ROLLBACK行为对DDL操作都失效.
+	DDL -> Database Definition Languages.
+	
+	实际上, 设计到COMMIT, ROLLBACK是有关数据库的事务, 在JDBC中讲述. 
+*/
 
 
 
