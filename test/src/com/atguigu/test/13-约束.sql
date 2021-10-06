83a20727633a955c4e14d36b69932f7f780a4edc
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-06 12:09:02 +0800

    13-约束

--- test/src/com/atguigu/test/13-约束.sql
+++ test/src/com/atguigu/test/13-约束.sql
@@ -28,6 +28,9 @@
 
 
 
+
+
+-- ----------------------------------------------------------------------------------------------------------
 /*01
     NOT NULL -> 非空约束
 */
@@ -70,17 +73,93 @@ MODIFY last_name VARCHAR(15) NULL;	# 最后加上一个null, 意思是在插入�
 
 
 
+-- ----------------------------------------------------------------------------------------------------------
+/*02
+    UNIQUE -> 唯一性约束
+	UNIQUE能写在列级约束(就是紧跟在字段后面), 也可以写在表级约束. 
+*/
+CREATE TABLE emp4(
+	id INT UNIQUE,
+	last_name VARCHAR(15),
+	email VARCHAR(25),
+	hire_date DATE,
+	# 表级约束
+	CONSTRAINT emp4_email_uk UNIQUE(email)
+);
+
+
+DESC emp4;
+
+SELECT * FROM emp4;
+
+
+# 成功 -> 插入数据
+INSERT INTO emp4(id,last_name,email,hire_date)
+VALUES(1,'Tom','tom@126.com',CURDATE());
+
+
+# 插入失败 -> Duplicate entry '1' for key 'id'
+INSERT INTO emp4(id,last_name,email,hire_date)
+VALUES(1,'Tom','tom@126.com',CURDATE());
+
+# 插入失败 -> Duplicate entry 'tom@126.com' for key 'emp4_email_uk'
+INSERT INTO emp4(id,last_name,email,hire_date)
+VALUES(2,'Jury','tom@126.com',CURDATE());
+
+# 插入成功
+INSERT INTO emp4(id,last_name,email,hire_date)
+VALUES(3,'Tom1',NULL,CURDATE());
+
+# 插入成功
+INSERT INTO emp4(id,last_name,email,hire_date)
+VALUES(4,'Tom1',NULL,CURDATE());
+
+SELECT * FROM emp4;
+
+/*
+    结论 -> 
+	声明为unique约束的字段, 在添加或修改数据时, 允许多次设置为null
+*/
 
 
 
+/*
+    在修改表的时候, 如果删除约束? 
+
+    1. 在创建唯一约束的时候, 如果不给唯一约束起别名, 那么就默认和列原来的名字相同.
+    2. MySQL会给唯一约束的列上默认创建一个唯一索引
+    
+    
+    删除约束
+	- 删除唯一约束只能通过删除唯一索引的方式删除。
+	- 删除时需要指定唯一索引名，唯一索引名就和唯一约束名一样。
+	- 如果创建唯一约束时未指定名称，如果是单列，就默认和列名相同，如果是组合列，那么默认和()中排在
+	  第一个的列名相同。也可以自定义唯一性约束名。
+
+*/
+
+DESC emp4;
+
+# 删除email字段的唯一性约束 <- 需要通过删除该字段的索引名称来删除
+ALTER TABLE emp4
+DROP INDEX emp4_email_uk;
+
+DESC emp4;
 
 
+# 再删除id的索引
+ALTER TABLE emp4
+DROP INDEX id;
+
+DESC emp4;
 
 
+# 重新再加上唯一性约束
+ALTER TABLE emp4
+ADD CONSTRAINT emp4_id_uk UNIQUE(id);
 
 
-
-
+DESC emp4;
 
 
 
